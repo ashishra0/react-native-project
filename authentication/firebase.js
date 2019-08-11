4e9737931a73a850d221eb900c23cde37a9a4929
Ashish Rao <ashishrao2598@gmail.com>
2019-08-11 11:23:16 +0530

    add firebase component

--- authentication/firebase.js
+++ authentication/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyDk88Eo95b6C2GBbLEkmbUEODmcnJorCbs",
+  apiKey: "",
   authDomain: "sicsr-dissertation-pep.firebaseapp.com",
   databaseURL: "https://sicsr-dissertation-pep.firebaseio.com",
   projectId: "sicsr-dissertation-pep",
   storageBucket: "sicsr-dissertation-pep.appspot.com",
-  messagingSenderId: "237240305407"
+  messagingSenderId: ""
 }
 
 const Firebase = firebase.initializeApp(config);
